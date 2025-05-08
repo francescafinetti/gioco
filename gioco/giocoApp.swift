954f6e0d856d96fa490f57ac9c1736b7d9f0475c
francescafinetti <francy.fine@icloud.com>
2025-05-08 16:38:50 +0200

    Initial Commit

---
+++ gioco/giocoApp.swift
@@ -0,0 +1,17 @@
+//
+//  giocoApp.swift
+//  gioco
+//
+//  Created by Francesca Finetti on 08/05/25.
+//
+
+import SwiftUI
+
+@main
+struct giocoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
