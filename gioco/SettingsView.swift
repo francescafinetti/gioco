0bc719cd4e60e6590c5b1a6a00721d67f86b1ede
francescafinetti <francy.fine@icloud.com>
2025-05-21 09:53:27 +0200

    impostazioni

---
+++ gioco/SettingsView.swift
@@ -0,0 +1,46 @@
+import SwiftUI
+
+struct SettingsView: View {
+    @AppStorage("volumeEnabled") private var volumeEnabled = true
+    @AppStorage("vibrationEnabled") private var vibrationEnabled = true
+    @AppStorage("isLeftHanded") private var isLeftHanded = false
+    @AppStorage("difficulty") private var difficulty = "Medium"
+    
+    let difficulties = ["Easy", "Medium", "Hard"]
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("Sound & Feedback")) {
+                    Toggle(isOn: $volumeEnabled) {
+                        Label("Volume", systemImage: "speaker.wave.2.fill")
+                    }
+                    
+                    Toggle(isOn: $vibrationEnabled) {
+                        Label("Vibrations", systemImage: "iphone.radiowaves.left.and.right")
+                    }
+                }
+                
+                Section(header: Text("Preferences")) {
+                    Picker("Mano dominante", selection: $isLeftHanded) {
+                        Text("Left hand").tag(false)
+                        Text("Right Hand").tag(true)
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    
+                    Picker("Difficulty", selection: $difficulty) {
+                        ForEach(difficulties, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Settings")
+        }
+    }
+}
+
+
+#Preview {
+       SettingsView()
+    }
